5e514e8945c39ec3a3b2ad2cd3fe7acbf9b2b956
asuraiv <asuraiv7@gmail.com>
2020-06-27 00:33:17 +0900

    카프카 연동
    - 설정 파일 수정 및 리스너 추가

--- src/main/kotlin/com/asuraiv/kotlinbackend/example/listener/SampleTopicListener.kt
+++ src/main/kotlin/com/asuraiv/kotlinbackend/example/listener/SampleTopicListener.kt
@@ -1,5 +1,18 @@
 package com.asuraiv.kotlinbackend.example.listener
 
+import org.apache.kafka.clients.consumer.ConsumerRecords
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Component
+
+@Component
 class SampleTopicListener {
 
+    val log = LoggerFactory.getLogger(SampleTopicListener::class.java)
+
+    @KafkaListener(topics = ["sample-topic"])
+    fun consume(records: ConsumerRecords<String, String>) {
+
+        log.info("Records count: ${records.count()}")
+    }
 }
