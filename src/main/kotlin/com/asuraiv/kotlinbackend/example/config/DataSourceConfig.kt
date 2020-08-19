bef25a3672be7d06c28d3a5f76ade9ed3abb1bd5
asuraiv <asuraiv7@gmail.com>
2020-08-19 23:40:19 +0900

    MyBatis 연동
    - 의존성 추가
    - configuration 코드 작성

--- src/main/kotlin/com/asuraiv/kotlinbackend/example/config/DataSourceConfig.kt
+++ src/main/kotlin/com/asuraiv/kotlinbackend/example/config/DataSourceConfig.kt
@@ -1,18 +1,31 @@
 package com.asuraiv.kotlinbackend.example.config
 
 import com.zaxxer.hikari.HikariDataSource
+import org.apache.ibatis.session.SqlSessionFactory
+import org.mybatis.spring.SqlSessionFactoryBean
+import org.mybatis.spring.annotation.MapperScan
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.sql.DataSource
 
+@Configuration
+// MyBatis Config
+@MapperScan(
+    basePackages = ["com.asuraiv.kotlinbackend.example"],
+    sqlSessionFactoryRef = "sessionFactory"
+)
+// JPA Config
 @EnableJpaRepositories(basePackages = ["com.asuraiv.kotlinbackend.example.domain"])
 @EntityScan(basePackages = ["com.asuraiv.kotlinbackend.example.domain"])
-@Configuration
+@EnableTransactionManagement
 class DataSourceConfig {
 
     @Primary
@@ -24,5 +37,18 @@ class DataSourceConfig {
                 .create()
                 .type(HikariDataSource::class.java)
                 .build()
+    }
+
+    @Primary
+    @Bean
+    fun sessionFactory(): SqlSessionFactory {
+
+        val sqlSessionFactoryBean = SqlSessionFactoryBean()
+
+        sqlSessionFactoryBean.setDataSource(dataSource())
+        sqlSessionFactoryBean.setMapperLocations(PathMatchingResourcePatternResolver().getResources("classpath:/mapper/*.xml"))
+        sqlSessionFactoryBean.vfs = SpringBootVFS::class.java
+
+        return sqlSessionFactoryBean.`object`!!
     }
 }
